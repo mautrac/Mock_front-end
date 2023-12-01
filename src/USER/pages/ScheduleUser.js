d37f74cabe9cf45141c8a2ae4b2e62e73ef83aa8
dai <dainb2002@gmail.com>
2023-12-01 20:21:54 +0700

    almost finish

--- src/USER/pages/ScheduleUser.js
+++ src/USER/pages/ScheduleUser.js
@@ -1,9 +1,20 @@
-import React from "react";
-import { ListGroup, ListGroupItem } from "reactstrap";
+import React, { useEffect, useState } from "react";
 
+import { Col, ListGroup, ListGroupItem, Row } from "reactstrap";
+import daysOfWeek from "../../utils/DaysOfWeek";
+import "../css/film_schedule.scss";
+//films
+import FilmList from "../component/FilmList";
+import scheduleApi from "../../api/ScheduleApi";
+import filmeApi from "../../api/FilmApi";
 
 const ScheduleUser = (props) => {
-    console.log("asdasd");
+
+    const [films, setFilms] = useState([]);
+    const [choosenDay, setChoosenDay] = useState(new Date());
+    const [indexActive, setIndexActive] = useState(0);
+    const [isActive, setActive] = useState([true]);
+    
     const now = new Date();
     var schedule = []
     schedule.push(now);
@@ -11,23 +22,101 @@ const ScheduleUser = (props) => {
         let temp = new Date();
         temp.setDate(temp.getDate() + i);
         schedule.push(temp);
+        isActive.push(false);
+    }
+    
+    useEffect(() => {
+        //console.log("mounted");
+        let filmIds = [];
+        
+        scheduleApi.getByDate(choosenDay)
+        .then((result) => {
+            result.content.forEach(element => {
+                filmIds.push(element.film.filmId);
+            });
+
+            //console.log(filmIds);
+            
+            const getFilms = async () => {
+                if (filmIds.length > 0) {
+                    setFilms([]);
+                    filmIds.forEach((id) => {
+                        filmeApi.getFilmById(id)
+                        .then((result) => {
+                            
+                            setFilms((old) => [...old, result]);
+                        })
+                        .catch((error) => {
+                            throw error;
+                        })
+        
+                    })
+                } else setFilms([]);
+                
+            }
+            getFilms();
+            
+        })
+        .catch((error) => {
+            throw error;
+        })
+
+        
+    }, [choosenDay]);
+
+    const handleClick = (e) => {
+        console.log(e);
     }
 
     return (
-        <>
-            {/* <ListGroup horizontal>
-                
-                {schedule.map((value) => {
+        <div className="schedule-page-container">
+        
+            <Col lg={9} >
+                <div className="schedule-page-container">
+                    <ul horizontal  className="schedule-page-list">
+                            {schedule.map((value, index) => {
+                                return (
+                                    <>
+                                        <li  key={index} className={"schedule-page-list-item"+ (isActive[index] ? " active-schedule": "")} 
+                                            onClick={() => {
+                                            setChoosenDay(value);
+                                            let newActive = [...isActive];
 
-                    (<ListGroupItem >
-                        <div>{value}</div>
-                    </ListGroupItem>)
-                })}
-            </ListGroup> */}
-            <div>
-                asdhuiahopsdasdhiopasdhjiopasdhioasdasdhioasdhioasdhio
-            </div>
-        </>
+                                            newActive[indexActive] = false;
+                                            newActive[index] = true;
+                                            setIndexActive(index);
+                                            setActive(newActive);
+                                        }}>
+                                                < a  >
+                                                    <h4 >
+                                                    {
+                                                        (() => {
+                                                            let d = new Date(value);
+                                                            let day = d.getDay();
+                                                            let date = `${d.getMonth() + 1}/${d.getDate()}`;
+                                                            return `${daysOfWeek[day]} - ${date}`;
+                                                        })()
+                                                    }
+                                                        
+                                                    </h4>
+                                                </a>
+                                            
+                                        </li>   
+                                    </>
+                                )    
+                            })}
+                    </ul>
+
+                </div>
+
+                
+            </Col>
+
+            <Col lg={10}>
+                <FilmList films = {films} />
+            </Col>
+
+        </div>
     )
 }
 
